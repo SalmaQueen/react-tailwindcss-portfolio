8455689213cf3ab52f1d824bcecdedde76f6ed62
salmaQueen <salmawasee9@gmail.com>
2023-06-09 00:34:10 +0300

    initial

---
+++ src/data.js
@@ -0,0 +1,91 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?auto=compress&cs=tinysrgb&w=1600",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?auto=compress&cs=tinysrgb&w=1600",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?auto=compress&cs=tinysrgb&w=1600",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?auto=compress&cs=tinysrgb&w=1600",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export const testimonials = [
+    {
+      quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      image: "https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      name: "John Doe",
+      company: "ABC Company"
+    },
+    {
+      quote: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem.",
+      image: "https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      name: "Jane Smith",
+      company: "XYZ Corporation"
+    },
+    {
+      quote: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut.",
+      image: "https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      name: "David Johnson",
+      company: "123 Industries"
+    },
+    {
+      quote: "Ut enim ad minima veniam, quis nostrum exercitationem ullam.",
+      image: "https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      name: "Emily Williams",
+      company: "456 Co."
+    },
+    {
+      quote: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      image: "https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      name: "Michael Brown",
+      company: "789 Corp"
+    }
+  ];
+  
+  export const skills = [
+    {
+      name: "JavaScript",
+      level: "Advanced"
+    },
+    {
+      name: "HTML",
+      level: "Intermediate"
+    },
+    {
+      name: "CSS",
+      level: "Intermediate"
+    },
+    {
+      name: "React",
+      level: "Advanced"
+    },
+    {
+      name: "Node.js",
+      level: "Intermediate"
+    }
+  ];
+  
